f6ec5f90f35ab5df68321e62b17159d9ef8e114a
jdi022222 <jdi022222@gmail.com>
2023-09-29 17:46:22 +0900

    chore: spring boot 프로젝트 생성 (#1)
    - devtools, lombok, web, data-jpa, webflux, security,data-redis, mariadb, validation

---
+++ src/main/java/com/meommu/meommuapi/MeommuApiApplication.java
@@ -0,0 +1,13 @@
+package com.meommu.meommuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeommuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeommuApiApplication.class, args);
+	}
+
+}
