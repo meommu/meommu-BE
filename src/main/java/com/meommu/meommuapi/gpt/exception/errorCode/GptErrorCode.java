bb727e339e8595f4caba898076c45b748c8d43c5
jdi022222 <jdi022222@gmail.com>
2023-10-09 23:23:54 +0900

    feat: GPT 일기 에러코드 생성 (#13)

---
+++ src/main/java/com/meommu/meommuapi/gpt/exception/errorCode/GptErrorCode.java
@@ -0,0 +1,17 @@
+package com.meommu.meommuapi.gpt.exception.errorCode;
+
+import com.meommu.meommuapi.common.exception.errorCode.ErrorCode;
+
+public enum GptErrorCode implements ErrorCode {
+	;
+
+	@Override
+	public String getCode() {
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return null;
+	}
+}
