21ac5b5fd9568cf724826b8657334a189ae3ed0a
jdi022222 <jdi022222@gmail.com>
2023-10-16 19:55:54 +0900

    feat: 유치원 회원 정보 조회. 로그인 정보 조회, 탈퇴 기능 구현 (#17)

--- src/main/java/com/meommu/meommuapi/kindergarten/controller/KindergartenController.java
+++ src/main/java/com/meommu/meommuapi/kindergarten/controller/KindergartenController.java
@@ -1,14 +1,20 @@
 package com.meommu.meommuapi.kindergarten.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.meommu.meommuapi.auth.dto.AuthInfo;
+import com.meommu.meommuapi.auth.token.Auth;
 import com.meommu.meommuapi.kindergarten.dto.EmailRequest;
+import com.meommu.meommuapi.kindergarten.dto.MyInfoBasicResponse;
+import com.meommu.meommuapi.kindergarten.dto.MyInfoResponse;
 import com.meommu.meommuapi.kindergarten.dto.SignUpRequest;
 import com.meommu.meommuapi.kindergarten.service.KindergartenService;
 
@@ -32,5 +38,21 @@ public class KindergartenController {
 	@GetMapping("/api/v1/kindergartens/email")
 	public void checkEmailDuplication(@Valid @ModelAttribute EmailRequest emailRequest) {
 		kindergartenService.existsByEmail(emailRequest);
+	}
+
+	@GetMapping("/api/v1/kindergartens/info")
+	public MyInfoResponse findKindergartenInfo(@Auth AuthInfo authInfo) {
+		return kindergartenService.findMyInfo(authInfo);
+	}
+
+	@GetMapping("/api/v1/kindergartens/info-basic")
+	public MyInfoBasicResponse findKindergartenInfoBasic(@Auth AuthInfo authInfo) {
+		return kindergartenService.findMyInfoBasic(authInfo);
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/api/v1/kindergartens/{kindergartenId}")
+	public void deleteKindergarten(@PathVariable Long kindergartenId, @Auth AuthInfo authInfo) {
+		kindergartenService.delete(kindergartenId, authInfo);
 	}
 }
