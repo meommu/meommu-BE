7def4579a011a8b2c06514c4617b553240c0a043
jdi022222 <jdi022222@gmail.com>
2023-10-23 16:33:05 +0900

    fix: 일기 조회시 @Valid가 적용되지 않는 문제 수정

--- src/main/java/com/meommu/meommuapi/diary/dto/DiarySearchCriteria.java
+++ src/main/java/com/meommu/meommuapi/diary/dto/DiarySearchCriteria.java
@@ -1,27 +1,23 @@
 package com.meommu.meommuapi.diary.dto;
 
-import java.time.LocalDate;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class DiarySearchCriteria {
 
-	@NotBlank(message = "연도는 필수로 입력해야 합니다.")
+	@NotNull(message = "연도는 필수로 입력해야 합니다.")
 	private Integer year;
 
-	@NotBlank(message = "월은 필수로 입력해야 합니다.")
+	@NotNull(message = "월은 필수로 입력해야 합니다.")
 	private Integer month;
 
 	private DiarySearchCriteria() {
 	}
 
 	@Builder
-	public DiarySearchCriteria(int year, int month) {
+	private DiarySearchCriteria(Integer year, Integer month) {
 		this.year = year;
 		this.month = month;
 	}
