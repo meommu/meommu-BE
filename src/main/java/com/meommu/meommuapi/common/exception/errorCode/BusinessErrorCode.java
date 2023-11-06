bd740f4d223396f59656b621fab93705744ff0d7
jdi022222 <jdi022222@gmail.com>
2023-11-06 15:22:31 +0900

    refactor: BusinessErrorCode 순서 수정

--- src/main/java/com/meommu/meommuapi/common/exception/errorCode/BusinessErrorCode.java
+++ src/main/java/com/meommu/meommuapi/common/exception/errorCode/BusinessErrorCode.java
@@ -6,9 +6,9 @@ public enum BusinessErrorCode implements ErrorCode {
 
 	//Client
 	BAD_REQUEST("C400", "잘못된 요청입니다. 요청내용을 확인하세요."),
-	NOT_FOUND("C404", "요청내용을 찾을 수 없습니다. 요청내용을 확인하세요."),
 	UNAUTHORIZED("C401", "인증되지 않았습니다. 인증을 확인하세요."),
 	FORBIDDEN("C403", "권한이 없습니다. 권한을 확인하세요."),
+	NOT_FOUND("C404", "요청내용을 찾을 수 없습니다. 요청내용을 확인하세요."),
 
 	//Application
 	INTERNAL_SERVER_ERROR("S500", "시스템 내부오류가 발생했습니다. 담당자에게 문의바랍니다."),
