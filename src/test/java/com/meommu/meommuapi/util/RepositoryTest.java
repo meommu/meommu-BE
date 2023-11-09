8a27d0f643c00e696e99916be7e490e5c68c6c21
jdi022222 <jdi022222@gmail.com>
2023-11-09 15:00:55 +0900

    test: 일기 repository 테스트 작성 (#33)

--- src/test/java/com/meommu/meommuapi/util/RepositoryTest.java
+++ src/test/java/com/meommu/meommuapi/util/RepositoryTest.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import com.meommu.meommuapi.common.config.JpaConfig;
+import com.meommu.meommuapi.diary.repository.DiaryImageRepository;
+import com.meommu.meommuapi.diary.repository.DiaryRepository;
 import com.meommu.meommuapi.kindergarten.domain.embedded.Encryptor;
 import com.meommu.meommuapi.kindergarten.repository.KindergartenRepository;
 
@@ -14,6 +16,12 @@ public abstract class RepositoryTest {
 
 	@Autowired
 	protected KindergartenRepository kindergartenRepository;
+
+	@Autowired
+	protected DiaryRepository diaryRepository;
+
+	@Autowired
+	protected DiaryImageRepository diaryImageRepository;
 
 	protected Encryptor encryptor = new Encryptor();
 }
