dc75a50e2e2ec83ef8c3bbdfeae101b1c4b03bc5
jdi022222 <jdi022222@gmail.com>
2023-11-14 15:51:31 +0900

    fix: 이미지 업로드 검증 로직 수정
    - 이미지를 업로드 하지 않을 경우 에러메세지가 뜨지 않는 문제 수정

--- src/main/java/com/meommu/meommuapi/image/dto/ImagesSaveRequest.java
+++ src/main/java/com/meommu/meommuapi/image/dto/ImagesSaveRequest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -15,7 +14,6 @@ public class ImagesSaveRequest {
 	@NotBlank(message = "카테고리는 반드시 입력해야 합니다.")
 	String category;
 
-	@NotNull(message = "이미지는 최소 1장 이상 업로드해야 합니다.")
 	List<MultipartFile> images;
 
 	@Builder
