32fe82fa1883a6b2e6503064af5bfbcaa0454a73
jdi022222 <jdi022222@gmail.com>
2023-11-25 19:31:00 +0900

    fix: CORS 설정에 HTTP 메서드 추가

--- src/main/java/com/meommu/meommuapi/common/config/WebMvcConfig.java
+++ src/main/java/com/meommu/meommuapi/common/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -28,7 +29,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("/html2canvas/proxy.json/**")
-			.allowedOrigins("*");
+			.allowedOrigins("*")
+			.allowedMethods(
+				HttpMethod.GET.name(),
+				HttpMethod.HEAD.name(),
+				HttpMethod.POST.name(),
+				HttpMethod.PUT.name(),
+				HttpMethod.PATCH.name(),
+				HttpMethod.DELETE.name());
 	}
 
 	@Override
