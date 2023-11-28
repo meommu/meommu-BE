aa8847532b366ab965c260d527924cf5f4081694
jdi022222 <jdi022222@gmail.com>
2023-11-28 14:20:07 +0900

    feat: gpt 일기 생성에 stream 방식 추가

--- src/main/java/com/meommu/meommuapi/gpt/controller/GptController.java
+++ src/main/java/com/meommu/meommuapi/gpt/controller/GptController.java
@@ -1,6 +1,7 @@
 package com.meommu.meommuapi.gpt.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +12,7 @@ import com.meommu.meommuapi.gpt.dto.GptGenerateResponse;
 import com.meommu.meommuapi.gpt.service.GptService;
 
 import jakarta.validation.Valid;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class GptController {
@@ -25,5 +27,11 @@ public class GptController {
 	@PostMapping("/api/v1/gpt")
 	public GptGenerateResponse createGptContent(@Valid @RequestBody GptGenerateRequest gptGenerateRequest) {
 		return gptService.createGptContent(gptGenerateRequest);
+	}
+
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(value = "/api/v1/gpt/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> createGptContentStream(@Valid @RequestBody GptGenerateRequest gptGenerateRequest) {
+		return gptService.createGptContentStream(gptGenerateRequest);
 	}
 }
--- src/main/java/com/meommu/meommuapi/gpt/service/GptService.java
+++ src/main/java/com/meommu/meommuapi/gpt/service/GptService.java
@@ -1,16 +1,26 @@
 package com.meommu.meommuapi.gpt.service;
 
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.meommu.meommuapi.gpt.dto.GptGenerateRequest;
 import com.meommu.meommuapi.gpt.dto.GptGenerateResponse;
 import com.meommu.meommuapi.gpt.dto.GptRequest;
+import com.meommu.meommuapi.gpt.dto.GptRequestStream;
 import com.meommu.meommuapi.gpt.dto.GptResponse;
 import com.meommu.meommuapi.gpt.exception.GptGenerateException;
 import com.meommu.meommuapi.gpt.util.GptClient;
+
+import reactor.core.publisher.Flux;
 
 @Transactional(readOnly = true)
 @Service
@@ -24,8 +34,17 @@ public class GptService {
 
 	public GptGenerateResponse createGptContent(GptGenerateRequest gptGenerateRequest) {
 		try {
-			GptResponse gptResponse = gptClient.chat(new GptRequest(gptGenerateRequest.getDetails()));
-			return GptGenerateResponse.from(extractContent(gptResponse));
+			GptResponse response = gptClient.chat(new GptRequest(gptGenerateRequest.getDetails()));
+			return GptGenerateResponse.from(extractContent(response));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			throw new GptGenerateException();
+		}
+	}
+
+	public Flux<String> createGptContentStream(GptGenerateRequest gptGenerateRequest) {
+		try {
+			return gptClient.chatStream(new GptRequestStream(gptGenerateRequest.getDetails()));
 		} catch (Exception e) {
 			throw new GptGenerateException();
 		}
