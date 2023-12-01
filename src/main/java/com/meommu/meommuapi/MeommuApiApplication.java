907c1ce066f12c9e9a231a324de75b0aad3eee5f
jdi022222 <jdi022222@gmail.com>
2023-12-01 17:10:24 +0900

    feat: 타임존 서울로 설정

--- src/main/java/com/meommu/meommuapi/MeommuApiApplication.java
+++ src/main/java/com/meommu/meommuapi/MeommuApiApplication.java
@@ -1,13 +1,22 @@
 package com.meommu.meommuapi;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class MeommuApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeommuApiApplication.class, args);
+	}
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
