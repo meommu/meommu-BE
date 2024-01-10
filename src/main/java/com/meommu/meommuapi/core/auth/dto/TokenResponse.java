5b453509205e7c7cbcd1ed43ad5eb84831ab6d34
jdi022222 <jdi022222@gmail.com>
2024-01-10 14:59:32 +0900

    feat: 로그아웃, JWT 토큰 재발급 로직 추가 (#53)

--- src/main/java/com/meommu/meommuapi/core/auth/dto/TokenResponse.java
+++ src/main/java/com/meommu/meommuapi/core/auth/dto/TokenResponse.java
@@ -8,16 +8,19 @@ public class TokenResponse {
 
 	private String accessToken;
 
+	private String refreshToken;
+
 	private TokenResponse() {
 	}
 
 	@Builder
-	private TokenResponse(String accessToken) {
+	private TokenResponse(String accessToken, String refreshToken) {
 		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
 	}
 
-	public static TokenResponse from(String accessToken) {
-		return new TokenResponse(accessToken);
+	public static TokenResponse from(String accessToken, String refreshToken) {
+		return new TokenResponse(accessToken, refreshToken);
 	}
 
 }
--- src/main/java/com/meommu/meommuapi/core/auth/service/AuthService.java
+++ src/main/java/com/meommu/meommuapi/core/auth/service/AuthService.java
@@ -1,14 +1,26 @@
 package com.meommu.meommuapi.core.auth.service;
 
-import org.springframework.stereotype.Service;
+import static com.meommu.meommuapi.core.auth.exception.errorCode.AuthErrorCode.*;
 
+import javax.naming.AuthenticationException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.meommu.meommuapi.core.auth.dto.AuthInfo;
+import com.meommu.meommuapi.core.auth.dto.ReissueRequest;
 import com.meommu.meommuapi.core.auth.dto.SignInRequest;
 import com.meommu.meommuapi.core.auth.dto.TokenResponse;
+import com.meommu.meommuapi.core.auth.exception.AuthorizationException;
+import com.meommu.meommuapi.core.auth.exception.JwtException;
 import com.meommu.meommuapi.core.auth.exception.SignInFailedException;
 import com.meommu.meommuapi.core.auth.token.JwtTokenProvider;
 import com.meommu.meommuapi.core.kindergarten.domain.Kindergarten;
 import com.meommu.meommuapi.core.kindergarten.domain.embedded.Encryptor;
 import com.meommu.meommuapi.core.kindergarten.repository.KindergartenRepository;
+import com.meommu.meommuapi.global.infra.redis.util.RedisUtils;
+
+import jakarta.security.auth.message.AuthException;
 
 @Service
 public class AuthService {
@@ -19,17 +31,57 @@ public class AuthService {
 
 	private final JwtTokenProvider jwtTokenProvider;
 
+	private final RedisUtils redisUtils;
+
 	public AuthService(KindergartenRepository kindergartenRepository, Encryptor encryptor,
-		JwtTokenProvider jwtTokenProvider) {
+		JwtTokenProvider jwtTokenProvider, RedisUtils redisUtils) {
 		this.kindergartenRepository = kindergartenRepository;
 		this.encryptor = encryptor;
 		this.jwtTokenProvider = jwtTokenProvider;
+		this.redisUtils = redisUtils;
 	}
 
-	public TokenResponse signIn(SignInRequest signInRequest) {
-		Kindergarten kindergarten = kindergartenRepository.findByEmailValueAndPasswordValue(signInRequest.getEmail(),
-			encryptor.encrypt(signInRequest.getPassword())).orElseThrow(() -> new SignInFailedException());
+	@Transactional
+	public TokenResponse signIn(SignInRequest request) {
+		Kindergarten kindergarten = kindergartenRepository.findByEmailValueAndPasswordValue(request.getEmail(),
+			encryptor.encrypt(request.getPassword())).orElseThrow(() -> new SignInFailedException());
 		String accessToken = jwtTokenProvider.createAccessToken(kindergarten.getId());
-		return TokenResponse.from(accessToken);
+		String refreshToken = jwtTokenProvider.createRefreshToken(kindergarten.getId());
+
+		Long expiration = jwtTokenProvider.getExpiration(refreshToken);
+		redisUtils.setRefreshToken(kindergarten.getId(), refreshToken, expiration);
+
+		return TokenResponse.from(accessToken, refreshToken);
+	}
+
+	@Transactional
+	public TokenResponse reissue(AuthInfo authInfo, ReissueRequest request) {
+		// 1. Refresh Token 검증
+		jwtTokenProvider.validateRefreshToken(request.getRefreshToken());
+
+		// 2. 인증정보
+		Long id = authInfo.getId();
+
+		// 3. Redis RefreshToken 조회
+		String refreshToken = redisUtils.getRefreshToken(id);
+		if (!refreshToken.equals(request.getRefreshToken())) {
+			throw new JwtException(MALFORMED_JWT);
+		}
+
+		// 3. 새로운 토큰 발급
+		String newAccessToken = jwtTokenProvider.createAccessToken(id);
+		String newRefreshToken = jwtTokenProvider.createRefreshToken(id);
+
+		// 4. Redis RefreshToken 업데이트
+		Long expiration = jwtTokenProvider.getExpiration(refreshToken);
+		redisUtils.setRefreshToken(id, newRefreshToken, expiration);
+
+		// 5. 토큰 반환
+		return TokenResponse.from(newAccessToken, newRefreshToken);
+	}
+
+	@Transactional
+	public void signOut(AuthInfo authInfo) {
+		redisUtils.deleteRefreshToken(authInfo.getId());
 	}
 }
