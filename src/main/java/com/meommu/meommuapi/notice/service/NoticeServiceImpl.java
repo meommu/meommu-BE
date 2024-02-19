88f001af8419a8272dcad346d5943404e24be428
jdi022222 <jdi022222@gmail.com>
2024-02-19 23:47:40 +0900

    fix: 캐시 주석 해제

--- src/main/java/com/meommu/meommuapi/notice/service/NoticeServiceImpl.java
+++ src/main/java/com/meommu/meommuapi/notice/service/NoticeServiceImpl.java
@@ -2,6 +2,7 @@ package com.meommu.meommuapi.notice.service;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,9 +21,8 @@ public class NoticeServiceImpl implements NoticeService{
 	}
 
 	@Override
-	// @Cacheable(cacheNames = "notice")
+	@Cacheable(cacheNames = "notice")
 	public NoticeResponses findNotices() {
-		System.out.println("캐시 이용 전");
 		List<Notice> notices = noticeRepository.findAllByOrderByCreatedAtDesc();
 		return NoticeResponses.from(notices);
 	}
